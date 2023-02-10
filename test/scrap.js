b6b51566110d90b8cf215e262d4b58348cdd5294
Aung Ko Man <aungkoman@gmail.com>
2023-02-10 13:28:13 +0530

    by using interval OK

--- test/scrap.js
+++ test/scrap.js
@@ -1,16 +1,58 @@
 
-var previousText = "";
-function myFunction() {
-    console.log("myFunction");
-    let text = $("#iChart-bodyLabels-cnt").text();
-    if(previousText != text){
-        console.log("Text changed");
-        console.log(text);
-        previousText = text;
-    }
-    else{
-        console.log("same text");
-    }
+
+function setLog(msg){
+    $("#description").append("<li>" + msg + "</li>");
 }
 
-setTimeout(myFunction, 1000);
+var previousText = "";
+function myFunction() {
+    let text = $("#iChart-bodyLabels-cnt").text();
+    if(previousText != text){
+        //setLog("Text changed");
+        //setLog(text);
+        previousText = text;
+        setLog(previousText);
+    }
+    else{
+        // setLog("same text");
+    }
+}
+myFunction();
+
+setInterval(myFunction, 1000);
+
+/*
+### console.log is disabled
+
+var logger = function()
+{
+    var oldConsoleLog = null;
+    var pub = {};
+
+    pub.enableLogger =  function enableLogger() 
+                        {
+                            if(oldConsoleLog == null)
+                                return;
+
+                            window['console']['log'] = oldConsoleLog;
+                        };
+
+    pub.disableLogger = function disableLogger()
+                        {
+                            oldConsoleLog = console.log;
+                            window['console']['log'] = function() {};
+                        };
+
+    return pub;
+}();
+
+console.log('hello');
+
+        logger.disableLogger();
+        console.log('hi', 'hiya');
+        console.log('this wont show up in console');
+
+        logger.enableLogger();
+        console.log('This will show up!');
+
+*/
